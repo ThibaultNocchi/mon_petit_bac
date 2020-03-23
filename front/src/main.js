c16a4af9efe0a8fcca13059cbec76eb439a7476b
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2020-03-23 18:32:15 +0100

    Front: install websocket module

--- front/src/main.js
+++ front/src/main.js
@@ -3,8 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import VueNativeSock from "vue-native-websocket";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueNativeSock, "ws://localhost:8081", { store: store });
 
 new Vue({
   router,
--- front/src/store/index.js
+++ front/src/store/index.js
@@ -5,7 +5,20 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
-  mutations: {},
+  mutations: {
+    SOCKET_ONOPEN(state, event) {
+      console.error(state, event);
+    },
+    SOCKET_ONCLOSE(state, event) {
+      console.error(state, event);
+    },
+    SOCKET_ONERROR(state, event) {
+      console.error(state, event);
+    },
+    SOCKET_ONMESSAGE(state, message) {
+      console.error(state, message);
+    }
+  },
   actions: {},
   modules: {}
 });
